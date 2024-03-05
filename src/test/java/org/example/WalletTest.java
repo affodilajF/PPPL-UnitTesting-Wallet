be9106e1a937a2b1b658420744d1198ba080b30c
Affodilaj Fadil <fadhilaainsalsabilla@mail.ugm.ac.id>
2024-03-05 21:19:57 +0700

    3

--- src/test/java/org/example/WalletTest.java
+++ src/test/java/org/example/WalletTest.java
@@ -1,75 +1,83 @@
 package org.example;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// Tugas pertemuan 3 => Menambahkan Life Cycle (Lifecylce.PER_METHOD)
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class WalletTest {
+    private Wallet wallet;
+    @BeforeEach
+    void initMethod(){
+        wallet = new Wallet("Aha", 120, new ArrayList<>(List.of("card1", "card2")));
+    }
+    @AfterEach
+    void cleanMethod(){
+        wallet = null;
+    }
+    @BeforeAll
+    static void initClass(){}
+
+    @AfterAll
+    static void cleanClass(){}
+
+
+
+    @Test
+    void testObjectCreation(){
+        String[] cardsExpected = {"card1", "card2"};
+        String[] cardsActual = wallet.getListKartu().toArray(new String[0]);
+
+        assertAll(
+                "Grouped Assertions of Wallet Object",
+                () -> assertEquals("Aha", wallet.getOwner()),
+                () -> assertEquals(120, wallet.getUangCash()),
+                () -> assertArrayEquals(cardsExpected, cardsActual)
+        );
+    }
 
     @Test
     void testWithdrawSufficientCash() {
-        final int UANG_AWAL = 120;
-        Wallet wallet = new Wallet("Aha", UANG_AWAL, new ArrayList<>());
-
         wallet.withdraw(10);
         assertEquals(110, wallet.getUangCash());
     }
 
     @Test
     void testWithdrawInsufficientCash() {
-        final int UANG_AWAL = 120;
-        Wallet wallet = new Wallet("Aha", UANG_AWAL, new ArrayList<>());
-
         assertThrows(Error.class, () -> {
             wallet.withdraw(1000);
-
         });
-
         assertEquals(120.0, wallet.getUangCash());
     }
-
-
     @Test
     void testDeposit() {
-        final int UANG_AWAL = 120;
-        Wallet wallet = new Wallet("Aha", UANG_AWAL, new ArrayList<>());
         wallet.deposit(30.0);
-
         assertEquals(150.0, wallet.getUangCash());
     }
-
-
     @Test
     void testAddCard() {
-        Wallet wallet = new Wallet("Aha", 120, new ArrayList<>());
-        wallet.addCard("card1");
-        wallet.addCard("card2");
+        wallet.addCard("card3");
+        wallet.addCard("card4");
         assertAll(
-                () -> assertTrue(wallet.getListKartu().contains("card1")),
-                () -> assertTrue(wallet.getListKartu().contains("card2"))
+                () -> assertTrue(wallet.getListKartu().contains("card3")),
+                () -> assertTrue(wallet.getListKartu().contains("card4"))
         );
     }
-
-
     @Test
     void testRemoveCard() {
-        ArrayList<String> cards = new ArrayList<>();
-        cards.add("card1");
-        Wallet wallet = new Wallet("Aha", 120, cards);
-
-        wallet.removeCard("card1");
-
-        assertFalse(wallet.getListKartu().contains("card1"));
+        wallet.removeCard("card2");
+        assertFalse(wallet.getListKartu().contains("card2"));
     }
-
-
     @Test
     void testRemoveNoneCard(){
-        Wallet wallet = new Wallet("Aha", 120, new ArrayList<>());
         assertThrows(Error.class, () -> wallet.removeCard("card5UUUU"));
     }
+
+
 }
